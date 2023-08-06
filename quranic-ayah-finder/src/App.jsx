3a5239a634181de527bae551f0c0dc5b4cac6268
Sidra Sajjad <sidra2611@hotmail.com>
2023-08-06 15:45:55 +0400

    basic navbar implementation

--- quranic-ayah-finder/src/App.jsx
+++ quranic-ayah-finder/src/App.jsx
@@ -1,13 +1,44 @@
 // import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 import { Searchbar } from './components/SearchBar/SearchBar'
+import "./components/Styles/styles.css" 
+import Home from './pages/Home/Home'
+import Guides from './pages/Guides/Guides'
+import FAQ from './pages/FAQs/FAQ'
+import DiscussionForum from './pages/DiscussionForum/DiscussionForum'
+import SignUp from './pages/SignUp/SignUp'
 
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  let Component
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home
+      break
+    case "/guides":
+      Component = Guides
+      break
+    case "/faq":
+      Component = FAQ
+      break
+    case "/discussionForums":
+      Component = DiscussionForum
+      break
+    case "/signup":
+      Component = SignUp
+      break
+    case "/login":
+      Component = SignUp
+      break
+  }
   return (
     <>
+      <Navbar />
+      <div className='container'>
+        <Component />
+      </div>
       <Searchbar />
     </>
   )
