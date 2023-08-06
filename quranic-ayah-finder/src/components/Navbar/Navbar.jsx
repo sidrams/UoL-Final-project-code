98623105de38b70875ee1171df1d698d89e13ecc
Sidra Sajjad <sidra2611@hotmail.com>
2023-08-06 22:35:00 +0400

    navigation bar styles

--- quranic-ayah-finder/src/components/Navbar/Navbar.jsx
+++ quranic-ayah-finder/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,39 @@
 import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 export default function Navbar() {
     return(
-        <nav className="nav">
-            <Link to="/" className="site-title">Visual Quranic Ayah Finder</Link>
+        <div className='p-10'>
+        <nav className="nav rounded-xl shadow text-sm font-medium">
+            <Link to="/" className="site-title pl-2 font-normal">Visual Quranic Ayah Finder</Link>
             <ul>
                 <CustomLink to="/">Home</CustomLink>
                 <CustomLink to="/guides">Guides</CustomLink>
                 <CustomLink to="/faq">FAQs</CustomLink>
                 <CustomLink to="/discussionForums">Discussion Forums</CustomLink>
-                <CustomLink to="/signup">Sign up</CustomLink>
-                <CustomLink to="/login">Login</CustomLink>
+                <CustomLink to="/signup" 
+                    className="bg-[#55BDB3] text-white py-1 px-3 rounded-md flex min-w-[95px] place-content-center
+                                hover:bg-white hover:text-[#55BDB3] hover:border-[#55BDB3] hover:border-[1.5px]"
+                >
+                    Sign up
+                </CustomLink>
+                <CustomLink to="/login" 
+                    className="bg-white text-[#55BDB3] border-[#55BDB3] border-[1.5px] py-1 px-3 rounded-md flex min-w-[95px] place-content-center
+                                hover:text-white hover:bg-[#F3F3F3] hover:border-[#55BDB3] hover:border-[1.5px]"
+                >
+                    Login
+                </CustomLink>
             </ul>
         </nav>
+        </div>
     )
 }
 
 function CustomLink({ to, children, ...props }) {
     const resolvedPath = useResolvedPath(to)
     const isActive = useMatch({path: resolvedPath.pathname, end: true})
+    const classes = props.className
 
     return (
-        <li className={isActive ? "active" : ""}>
+        <li className={isActive ? ("active " + classes): classes}>
             <Link to={to}>{children}</Link>
         </li>
     )
