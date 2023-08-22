cd24ac0ed3bd3af9c1650e5a648a82ed29244f2b
Sidra Sajjad <sidra2611@hotmail.com>
2023-08-22 18:14:23 +0400

    user logout implementation

--- quranic-ayah-finder/src/pages/SignUp/SignUp.jsx
+++ quranic-ayah-finder/src/pages/SignUp/SignUp.jsx
@@ -7,57 +7,46 @@ export default function SignUp() {
         password: ''
     })
     const csrftoken = Cookies.get('csrftoken');
+    const [loggedUser, setLoggedUser] = useState()
 
     const login = () => {
-        // let data = new FormData();
-        // data.append('username', form.username)
-        // data.append('password', form.password)
-        // data.append('fileName', file.name);
-        // // add form input from hidden input elsewhere on the page
-        // data.append('csrfmiddlewaretoken', $('#csrf-helper input[name="csrfmiddlewaretoken"]').attr('value'));
-        // fetch("/upload/", {
-        //     method: 'POST',
-        //     body: data,
-        //     credentials: 'same-origin',
-        // })
-        // console.log(form)
         fetch(`http://127.0.0.1:8000/login`, {
-            // method: "POST",
-            // headers: {
-            //     'Accept': 'application/json',
-            //     'Content-Type': 'application/json; charset=UTF-8',
-            //     'X-CSRFToken': get_token
-            // },
-            // credentials: 'include',
             method: 'POST',
-            // mode: 'same-origin',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
                 'X-CSRFToken': csrftoken,
             },
             credentials: 'include',
-            // true,
             body: 
             JSON.stringify(form),
-            // {
-            //     username : form.username,
-            //     password : form.password
-            // }
-            // JSON.stringify(form),
-            // data
-            // 
-            // method: 'POST',
-            // body: data,
-            // credentials: 'same-origin',
         })
         .then((response) => response.json())
         .then((json) =>{
             console.log(json)
-            // redirect('/success')
-            if (json == 'User Logged in') setSuccess(true)
+            setLoggedUser(json)
         })
-        .catch(error => console.log(error))
+        .catch(error => {console.log(error)})
+    }
+
+    const logout = () => {
+        fetch(`http://127.0.0.1:8000/logout`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrftoken,
+            },
+            credentials: 'include',
+            // body: 
+            // JSON.stringify(form),
+        })
+        .then((response) => setLoggedUser(null))
+        // .then((response) =>{
+        //     console.log(json)
+        //     setLoggedUser(null)
+        // })
+        .catch(error => {console.log(error)})
     }
 
     const handleChange = (e) => {
@@ -65,15 +54,39 @@ export default function SignUp() {
             ...form,
             [e.target.name] : e.target.value
         })
-        // console.log(form)
     }
 
     return (
         <>
         <h1>SignUp</h1>
-        <div>
+        {
+            loggedUser ? 
+            (
+                <>
+                <h2>USer logged in</h2>
+                <button onClick={logout}>logout</button>
+                </>
+            ) 
+            : 
+            (
+                <div>
+                    <form method="POST" action="">
+                        {/* <input type="hidden" name="csrfmiddlewaretoken" value={csrftoken} /> */}
+                        
+                        <label>Username:</label>
+                        <input type="text" value={form.username} onChange={handleChange} name="username" placeholder="Enter Username..." />
+
+                        <label>Password::</label>
+                        <input type="password" value={form.password} onChange={handleChange} name="password" placeholder="Enter Password..." />
+
+                        <input type="button" value="Login" onClick={login} />
+                    </form>
+                </div>
+            )
+        }
+        {/* <div>
             <form method="POST" action="">
-                <input type="hidden" name="csrfmiddlewaretoken" value={csrftoken} />
+                {/* <input type="hidden" name="csrfmiddlewaretoken" value={csrftoken} /> 
                 
                 <label>Username:</label>
                 <input type="text" value={form.username} onChange={handleChange} name="username" placeholder="Enter Username..." />
@@ -83,7 +96,7 @@ export default function SignUp() {
 
                 <input type="button" value="Login" onClick={login} />
             </form>
-        </div>
+        </div> */}
         </>
     )
 }
