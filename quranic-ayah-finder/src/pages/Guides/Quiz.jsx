c6c529d579a4f05d27fa381f0d5eaac3b41b6807
Sidra Sajjad <sidra2611@hotmail.com>
2023-08-24 16:16:11 +0400

    quiz code refactoring

--- quranic-ayah-finder/src/pages/Guides/Quiz.jsx
+++ quranic-ayah-finder/src/pages/Guides/Quiz.jsx
@@ -7,66 +7,10 @@ export default function Quiz(props) {
     const [currentQuestion, setCurrentQuestion] = useState()
     const [chosenAnswer, setChosenAnswer] = useState()
     const [score, setScore] = useState(0)
-    const [color, setColor] = useState({
-        // answer_1: false,
-        // answer_2: false,
-        // answer_3: false,
-        // answer_4: false,
-    })
     const [disabled, setDisabled] = useState(false)
     const [endQuiz, setEndQuiz] = useState(false)
 
-    // const location = useLocation();
-    // console.log(props, " props");
-    // console.log(location, " useLocation Hook");
-    // const data = location.state?.data;
-    const nextQuestion = () => {
-        const index = quizQuestions.indexOf(currentQuestion);
-        console.log(index)
-        if(index >= 0 && index < quizQuestions.length - 1) {
-        //    nextItem = array[index + 1]
-            setCurrentQuestion(quizQuestions[index + 1])
-            setColor({})
-            setDisabled(false)
-        }
-        else {
-            setEndQuiz(true)
-        }
-    }
-
-    const handleClick = (e,answer) => {
-        console.log(e.target.value, "in handle Click")
-        // console.log(color && currentQuestion.answer_1 == currentQuestion.correct_answer, 'codition is?')
-        setChosenAnswer(e.target.value)
-        setColor({
-            // ...color,
-            [answer]:true
-        })
-        console.log(color)
-        setDisabled(true)
-        if(e.target.value == currentQuestion.correct_answer) {
-            setScore(score + 1)
-            console.log(score , "score")
-        }
-    }
-
-    const getColor = (chosenAnswer) => {
-        // let bgColor = ''
-        // console.log(chosenAnswer == currentQuestion.correct_answer, 'correct answer?')
-        const bgColor = chosenAnswer == currentQuestion.correct_answer ?  'bg-emerald-200' : 'bg-red-100'
-        return bgColor
-    }
-
-    const restartQuiz = () => {
-        setEndQuiz(false)
-        setScore(0)
-        setCurrentQuestion(quizQuestions[0])
-        setChosenAnswer(null)
-        setColor({})
-        setDisabled(false)
-    }
-
-    console.log("id id: "+id)
+    // get questions for the chosen topic
     useEffect(() => {
         fetch('http://127.0.0.1:8000/api/quizQuestions/'+id, {
             method: 'GET',
@@ -79,24 +23,48 @@ export default function Quiz(props) {
             .catch(error => console.log(error))
     }, [])
 
-    // console.log(quizQuestions)
-    console.log(currentQuestion)
+    // display next question when pressed
+    const nextQuestion = () => {
+        const index = quizQuestions.indexOf(currentQuestion);
+        console.log(index)
+        if(index >= 0 && index < quizQuestions.length - 1) {
+            setCurrentQuestion(quizQuestions[index + 1])
+            setChosenAnswer(null)
+            setDisabled(false)
+        }
+        else {
+            setEndQuiz(true)
+        }
+    }
+
+    // display if clicked answer is correct or incorrect
+    const handleClick = (e,answer) => {
+        setChosenAnswer(e.target.value)
+        setDisabled(true)
+        if(e.target.value == currentQuestion.correct_answer) {
+            setScore(score + 1)
+            console.log(score , "score")
+        }
+    }
+
+    // get color for button to be displayed
+    const getColor = (chosenAnswer) => {
+        const bgColor = chosenAnswer == currentQuestion.correct_answer ?  'bg-emerald-200' : 'bg-red-100'
+        return bgColor
+    }
+
+    // reset all variables to restart quiz
+    const restartQuiz = () => {
+        setEndQuiz(false)
+        setScore(0)
+        setCurrentQuestion(quizQuestions[0])
+        setChosenAnswer(null)
+        setDisabled(false)
+    }
+
     return (
         <>
         <h1>Quiz</h1>
-        {/* <h1> {data ? data : "Go to Home"} </h1> */}
-        {/* {
-            quizQuestions ? 
-            (   
-                <>
-                <div>
-                    {quizQuestions.map((question,i) => (
-                        <h3>{question.question}</h3>
-                    ))}
-                </div>
-                </>
-            ) :''
-        } */}
         {
             currentQuestion ? (!endQuiz ? 
             // ("quiz has not ended") 
@@ -104,31 +72,15 @@ export default function Quiz(props) {
                 <div className="bg-[#55BDB3] p-6 py-8 w-[60%] m-auto rounded-xl">
                     <h3>{currentQuestion.question}</h3>
                     <div className="flex flex-col gap-4 px-24 my-4">
-                        <button 
-                            value={currentQuestion.answer_1} 
-                            disabled={disabled}
-                            // className={(color && currentQuestion.answer_1 == currentQuestion.correct_answer) ? 'bg-emerald-200' : 'bg-red-100'}
-                            // className={color ? (currentQuestion.answer_1 == currentQuestion.correct_answer ? 'bg-emerald-200' : 'bg-red-100'):''}
-                            className={color.answer_1 ? getColor(currentQuestion.answer_1) : ''}
-                            // className={(e) => (e.target.value)}
-                            onClick={(e) => {
-                                // console.log(e.target.value)
-                                console.log(color)
-                                // getColor(currentQuestion.answer_1)
-                                // e.target.value == currentQuestion.correct_answer ? setColor('emerald-200') : setColor('red-100')
-                                handleClick(e,'answer_1')
-                            }}
-                        >
-                            {currentQuestion.answer_1}
-                        </button>
-                        <button value={currentQuestion.answer_2} disabled={disabled} className={color.answer_2 ? getColor(currentQuestion.answer_2) : ''} onClick={(e) => handleClick(e,'answer_2')}>{currentQuestion.answer_2}</button>
-                        <button value={currentQuestion.answer_3} disabled={disabled} className={color.answer_3 ? getColor(currentQuestion.answer_3) : ''} onClick={(e) => handleClick(e,'answer_3')}>{currentQuestion.answer_3}</button>
-                        <button value={currentQuestion.answer_4} disabled={disabled} className={color.answer_4 ? getColor(currentQuestion.answer_4) : ''} onClick={(e) => handleClick(e,'answer_4')}>{currentQuestion.answer_4}</button>
+                        <button value={currentQuestion.answer_1} disabled={disabled} className={chosenAnswer == currentQuestion.answer_1 ? getColor(currentQuestion.answer_1) : ''} onClick={(e) => handleClick(e,'answer_1')}>{currentQuestion.answer_1}</button>
+                        <button value={currentQuestion.answer_2} disabled={disabled} className={chosenAnswer == currentQuestion.answer_2 ? getColor(currentQuestion.answer_2) : ''} onClick={(e) => handleClick(e,'answer_2')}>{currentQuestion.answer_2}</button>
+                        <button value={currentQuestion.answer_3} disabled={disabled} className={chosenAnswer == currentQuestion.answer_3 ? getColor(currentQuestion.answer_3) : ''} onClick={(e) => handleClick(e,'answer_3')}>{currentQuestion.answer_3}</button>
+                        <button value={currentQuestion.answer_4} disabled={disabled} className={chosenAnswer == currentQuestion.answer_4 ? getColor(currentQuestion.answer_4) : ''} onClick={(e) => handleClick(e,'answer_4')}>{currentQuestion.answer_4}</button>
                     </div>
                     <div>
                     {
-                        disabled ? (chosenAnswer == currentQuestion.correct_answer ? 
-                            ("Correct! " + currentQuestion.note) : ("Wrong... "+ currentQuestion.note)) : ''
+                        disabled ? ((chosenAnswer == currentQuestion.correct_answer ? 
+                            ("Correct! ") : ("Wrong... ")) + (currentQuestion.note ? currentQuestion.note : '')) : ''
                     }
                     </div>
                     <button onClick={nextQuestion}>Next</button>
