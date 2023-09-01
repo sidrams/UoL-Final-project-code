12d65de34139319ddba0cb50ab2ff9e3d30c2b09
Sidra Sajjad <sidra2611@hotmail.com>
2023-09-01 08:09:46 +0400

    completed profile page

---
+++ quranic-ayah-finder/src/components/Profile/ProfileHeader.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import BackButton from "../Buttons/BackButton";
+import { RiNumbersFill } from "react-icons/ri";
+import { useContext } from "react";
+import { Context } from "../../Context";
+
+export default function ProfileHeader() {
+    const { loggedUser, setLoggedUser } = useContext(Context)
+    
+    // username of user and link to user progress page ----------
+    return (
+        <div className="flex justify-between">
+            <div className='flex flex-col justify-between items-start tracking-wide w-3/4'>
+                <h1 className="text-2xl font-semi text-left">
+                    Your Profile
+                </h1>
+                <p className="text-sea-green text-lg mt-3">
+                    Logged in as  
+                    <span className="font-bold">
+                        {" @" +loggedUser.username}
+                    </span>
+                </p>
+            </div>
+
+            <Link to="/profile/scores"><BackButton text="View All scores" icon={<RiNumbersFill />}/></Link>
+        </div>
+    )
+}
--- quranic-ayah-finder/src/pages/Profile/Profile.jsx
+++ quranic-ayah-finder/src/pages/Profile/Profile.jsx
@@ -5,12 +5,21 @@ import ProfileTiles from "../../components/Profile/ProfileTiles"
 import Cookies from "js-cookie"
 import BackButton from "../../components/Buttons/BackButton"
 import { RiNumbersFill, RiTimeFill,RiTrophyFill } from 'react-icons/ri'
+import PostComponent from "../../components/Posts/PostComponent"
+import { BiSearchAlt2 } from "react-icons/bi"
+import ProfileHeader from "../../components/Profile/ProfileHeader"
+import AttemptedTopicList from "../../components/ProfileScore/AttemptedTopicList"
 
 export default function Profile() {
     const { loggedUser, setLoggedUser } = useContext(Context)
     const csrftoken = Cookies.get('csrftoken');
     const [profileData, setProfileData] = useState()
+    const [inputText, setInputText] = useState()
+    const handleChange = (e) => {
+        setInputText(e.target.value.toLowerCase())
+    }
 
+    // get profile data
     useEffect(() => {
         fetch('http://127.0.0.1:8000/profile', {
             method: 'GET',
@@ -29,61 +38,61 @@ export default function Profile() {
             .catch(error => console.log(error))
     }, [])
 
-    console.log(loggedUser)
+    const getTopicsAttempted = () => {
+        let topics = []
+        profileData.progress.map((quiz,i) => {
+            topics.push(quiz.quiz_topic_id.topic_name)
+        })
+        return([... new Set(topics)])
+    }
+
     return (
-        <>
-        {/* <h1>Profile</h1>
-        <p>hi {loggedUser.username}</p> */}
-
-        {/* <div>
-            see your scores <Link to="scores">here</Link>
-        </div> */}
-
         <div className="xl:w-[70%] lg:w-[85%] m-auto flex flex-col gap-4">
-            {/* <h1 className="hidden">Scores</h1> */}
-            
-            <div className="flex justify-between">
-                <div className='flex flex-col justify-between items-start tracking-wide w-3/4'>
-                    <h1 className="text-2xl font-semi text-left">
-                        Your Profile
-                    </h1>
-                    <p className="text-sea-green text-lg mt-3">
-                        Logged in as  
-                        <span className="font-bold">
-                            {" @" +loggedUser.username}
-                        </span>
-                    </p>
-                </div>
 
-                <Link to="/profile/scores"><BackButton text="View All scores" icon={<RiNumbersFill />}/></Link>
-            </div>
+            {/* username of user and link to user progress page */}
+            <ProfileHeader />
 
-            {/* summary of completed attempts */}
             {
-                profileData && <ProfileTiles profileData={profileData} />
-            }
+                profileData && 
+                (
+                    <>
+                    {/* summary of completed attempts */}
+            {/* {
+                profileData && 
+            } */}
 
+            <ProfileTiles profileData={profileData} />
+            
             <div className='flex flex-col w-full'>
                 <div className='flex justify-between my-6 tracking-wide text-gray-500 w-3/4'>
                     <h2 className=''>
                         Showing all posts for <span className='font-bold'>{loggedUser.username}</span>
                     </h2>
-                    <p>sort by <span className='font-bold'>*</span></p>
+
+                    {/* search bar to search user's posts */}
+                    <form method="GET" action="" className="w-[40%] flex items-center gap-1">
+                        <BiSearchAlt2 className="text-[1.5rem] text-sea-green" /><input type="text" className='w-full shadow' name="q" placeholder="Search posts..." onChange={handleChange} />
+                    </form>
                 </div>
 
-                {/* all progress components */}
+                {/* all posts components */}
                 <div className='flex'>
                     
-                    {/* indivisual progress components */}
+                    {/* indivisual posts components */}
+                    {/* {profileData && } */}
+                    <PostComponent posts={profileData.posts} inputText={inputText} />
                     {/* <IndivisualProgressComponent scoreData={scoreData} /> */}
 
                     {/* List of topics that quizzes have been attempted for */}
-                    {/* <AttemptedTopicList getTopicsAttempted={getTopicsAttempted} /> */}
+                    <AttemptedTopicList getTopicsAttempted={getTopicsAttempted} />
                 </div>
 
                 
             </div>
+                    </>
+                )
+            }
+            
         </div>
-        </>
     )
 }
