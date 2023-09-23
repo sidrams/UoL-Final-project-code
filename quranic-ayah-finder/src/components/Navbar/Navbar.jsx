27367e5258b8aa0dfedf011e22915e61907fa9a2
Sidra Sajjad <sidra2611@hotmail.com>
2023-09-23 19:58:32 +0400

    navbar design edits

--- quranic-ayah-finder/src/components/Navbar/Navbar.jsx
+++ quranic-ayah-finder/src/components/Navbar/Navbar.jsx
@@ -11,16 +11,16 @@ export default function Navbar() {
         <nav className="nav rounded-xl shadow text-sm font-medium ">
             <Link to="/" className="site-title pl-2 font-normal">Visual Quranic Ayah Finder</Link>
             <FiMenu className='lg:hidden block h-6 w-6 cursor-pointer' onClick={() => setOpen(!open)} />
-            <ul className={`lg:flex ${open ? 'hidden' : 'hidden'} p-0 m-0 list-none gap-4`}>
+            <ul className={`lg:flex hidden p-0 m-0 list-none gap-4`}>
                 <NavbarLinks setOpen={setOpen} />
             </ul>
         </nav>
 
         {/* mobile navigation menu */}
         <div className={`${open ? 'flex' : 'hidden'} bg-sea-green fixed w-full h-full top-0 z-[1000] left-0 `}>
-            <ul className={`w-full flex flex-col  p-0 m-0 list-none gap-6 pb-8 px-2 text-xl`}>
+            <ul className={`w-full flex flex-col justify-between p-0 m-0 list-none gap-6 pb-16 px-2 text-3xl`}>
                 <div className=' w-full flex justify-end pt-4'>
-                    <FiX className='h-6 w-6 text-white justify-end' onClick={() => setOpen(!open)} />
+                    <FiX className='h-10 w-10 text-white justify-end' onClick={() => setOpen(!open)} />
                 </div>
                 <NavbarLinks setOpen={setOpen} />
             </ul>
